aa9abbe92032a8d7b3f4f4b23f12d7af96dab347
shivam-1403 <thakralshivam21@gmail.com>
2024-12-05 11:14:23 +0530

    if else and conditional operators

---
+++ If_Else_Conditiona_Operators/if_else_basic.py
@@ -0,0 +1,8 @@
+# This program will take the height of a person as input, to check whether he can ride a roller coaster on not
+
+print("Welcome to the Roller Coaster !")
+height=int(input("Enter Your Height in cms : "))
+if height>120:
+    print("You can ride the Roller Coaster")
+else:
+    print("Sorry, you have to grow taller before you ride")
