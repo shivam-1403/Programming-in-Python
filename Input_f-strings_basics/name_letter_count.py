48b1ae54f9b01e0d932a456d6bf484bf0f0c06ee
shivam-1403 <thakralshivam21@gmail.com>
2024-12-05 10:56:26 +0530

    Update name_letter_count.py
    code update

--- Input_f-strings_basics/name_letter_count.py
+++ Input_f-strings_basics/name_letter_count.py
@@ -1,6 +1,2 @@
 name=input("What is your name ?\n")
 print("Your name's letter count is : "+str(len(name)))
-
-#or
-
-print("Your name's letter count is : "+ " "+str(len(input("What is your name ? \n"))))
