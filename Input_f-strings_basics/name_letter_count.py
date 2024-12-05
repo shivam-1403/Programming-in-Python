9c459f92b022319b6901a45f9a44b798072c58aa
shivam-1403 <thakralshivam21@gmail.com>
2024-12-05 10:50:51 +0530

    Add New Folder

---
+++ Input_f-strings_basics/name_letter_count.py
@@ -0,0 +1,6 @@
+name=input("What is your name ?\n")
+print("Your name's letter count is : "+str(len(name)))
+
+#or
+
+print("Your name's letter count is : "+ " "+str(len(input("What is your name ? \n"))))
