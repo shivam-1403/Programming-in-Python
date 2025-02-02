e0c47cbc5b008a78ffd6047958073518a1160d8b
shivam-1403 <thakralshivam21@gmail.com>
2025-02-02 17:41:52 +0530

    new

---
+++ 032.Password_Manager_App/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+        # with open(r"D:\Python Code\Programming-in-Python\032.Password_Manager_App\data.json", "r") as data_file:
+        #     # Reading old data:
+        #     data = json.load(data_file)
+        #     # Updating old data:
+        #     data.update(new_data)
